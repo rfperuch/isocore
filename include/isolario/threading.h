8582aa4e16b370a41f66c3a247211d53874aa022
Lorenzo Cogotti <micia@inventati.org>
2018-04-10 14:36:06 +0200

    Added pool and threading utilities to isocore, borrowed from ice

---
+++ include/isolario/threading.h
@@ -0,0 +1,71 @@
+//
+// Copyright (c) 2018, Enrico Gregori, Alessandro Improta, Luca Sani, Institute
+// of Informatics and Telematics of the Italian National Research Council
+// (IIT-CNR). All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE IIT-CNR BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+/**
+ * @file isolario/threading.h
+ *
+ * @brief Thread and pool utilities
+ *
+ * @note This file is guaranteed to include standard \a stddef.h,
+ *       a number of other files may be included in the interest of providing
+ *       API functionality, but the includer of this file
+ *       should not rely on such behavior.
+ */
+
+#ifndef ISOLARIO_THREADING_H_
+#define ISOLARIO_THREADING_H_
+
+#include <stddef.h>
+
+typedef struct pool_s pool_t;
+
+/**
+ * @brief Retrieve a platform-specific integral unique descriptor of the calling
+ *        thread.
+ *
+ * @return The platform specific integral descriptor of the calling thread.
+ *         If this platform does not provide any mean of retrieving such
+ *         descriptor, then 0 is returned, and \a errno is set to \a ENOTSUP.
+ *
+ * @warning Since 0, in theory, is a valid platform specific descriptor for a
+ *          thread, the caller should set \a errno to 0 and check it afterwards
+ *          to ensure this function was successful.
+ */
+unsigned long long getthreaddescr(void);
+
+pool_t *pool_create(int nthreads, void (*handle_job)(void *job));
+
+int pool_nthreads(pool_t *pool);
+
+int pool_dispatch(pool_t *pool, const void *job, size_t size);
+
+void pool_join(pool_t *pool);
+
+#endif
---
+++ src/threading.c
@@ -0,0 +1,68 @@
+//
+// Copyright (c) 2018, Enrico Gregori, Alessandro Improta, Luca Sani, Institute
+// of Informatics and Telematics of the Italian National Research Council
+// (IIT-CNR). All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE IIT-CNR BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include <errno.h>
+#include <isolario/threading.h>
+#include <pthread.h>
+#if defined(__MACOSX__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#define USE_THREADID
+#include <pthread_np.h>
+#endif
+#if defined(__linux__)
+#include <unistd.h>
+#include <sys/syscall.h>
+
+extern long syscall(long number, ...);
+#endif
+
+unsigned long long getthreaddescr(void)
+{
+#if defined(USE_THREADID)
+    return pthread_threadid_np();
+#elif defined(__linux__)
+    return syscall(SYS_gettid);
+#else
+    union {
+        pthread_t handle;
+        unsigned long ul;
+        unsigned long long ull;
+    } descr;
+
+    descr.handle = pthread_self();
+    if (sizeof(handle) == sizeof(descr.ul))
+        return descr.ul;
+    if (sizeof(handle) == sizeof(descr.ull))
+        return descr.ull;
+
+    errno = ENOTSUP;
+    return 0;
+
+#endif
+}
