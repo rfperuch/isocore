d97980dff97c47799d6c3d8e038450c8563b3b21
Lorenzo Cogotti <micia@inventati.org>
2018-04-20 19:40:22 +0200

    Refactor sopen() and fix md5sig

--- src/sockets.c
+++ src/sockets.c
@@ -34,6 +34,7 @@
 #include <fcntl.h>
 #include <isolario/sockets.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -71,53 +72,55 @@ int socketflags(int fd, int flags)
 
 int setmd5key(int sd, char *md5key)
 {
-    #ifdef __linux__
+#ifdef __linux__
+
     struct sockaddr_storage addr;
     socklen_t len = sizeof(addr);
     if (getpeername(sd, (struct sockaddr*)&addr, &len) < 0)
         return -1;
- 
+
     struct tcp_md5sig md5sig;
     memset(&md5sig, 0, sizeof(md5sig));
     md5sig.tcpm_addr = addr;
     strcpy(md5sig.tcpm_key, md5key);
-    
+
     return setsockopt(sd, IPPROTO_TCP, TCP_MD5SIG, &md5sig, sizeof(md5sig));
-    #else
-        #error Please provide an implementation for that OS!
-    #endif
+
+#else
+    #error Please provide an implementation for that OS!
+#endif
 }
 
 int sopen(const struct sockaddr *addr, socklen_t addrlen, const char *options, ...)
 {
-    va_list va;
-    va_start(va, options);
-
     struct sockaddr *saddr = NULL;
     socklen_t saddrlen;
     bool should_listen = false;
-    
+
     int backlog = 0;
     int family = AF_INET;
     int type = SOCK_STREAM;
     int reuse = 0;
     int flags = 0;
     int err;
-    
+
+    va_list va;
+    va_start(va, options);
+
     while (*options != '\0') {
         switch (*options++) {
         case '4':
             family = AF_INET;
-            
             break;
+
         case '6':
             family = AF_INET6;
-            
             break;
+
         case '#':
             type = SOCK_DGRAM;
-            
             break;
+
         case 'b':
             if (*options == '*') {
                 backlog = va_arg(va, int);
@@ -125,61 +128,58 @@ int sopen(const struct sockaddr *addr, socklen_t addrlen, const char *options, .
                 backlog = atoi(options);
                 do options++; while (isdigit(*options));
             }
-            
+
             break;
-        
+
         case 'C':
             saddr = va_arg(va, struct sockaddr *);
             saddrlen = va_arg(va, socklen_t);
-            
+            should_listen = false;
             break;
-        
+
         case 'c':
-        
+            should_listen = false;
             break;
-       
-        
+
         case 'l':
             should_listen = true;
-                
             break;
-            
+
         case 'n':
             flags |= O_NONBLOCK;
-                
             break;
-                
-        
+
         case 'r':
             reuse = 1;
-            
             break;
-        
+
         case 'u':
             family = AF_UNIX;
-            
             break;
 
         default:
+            va_end(va);
             errno= EINVAL;
             return -1;
         }
     }
-    
+
+    va_end(va);
+
     int fd = socket(family, type, 0);
     if (fd < 0)
-        return fd;
-    
+        return -1;
+
     if (reuse && setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) < 0)
         goto error;
-    
+
     if (flags && socketflags(fd, flags) < 0)
         goto error;
-    
+
     if (should_listen) {
         if (backlog < 1)
             backlog = DEFAULT_BACKLOG;
-            
+
         if (listen(fd, backlog) < 0)
             goto error;
     } else {
@@ -188,17 +188,14 @@ int sopen(const struct sockaddr *addr, socklen_t addrlen, const char *options, .
 
         if (connect(fd, addr, addrlen) < 0 && errno != EINPROGRESS)
             goto error;
-            
     }
 
-    va_end(va);
     return fd;
 
 error:
     err = errno;
     close(fd);
     errno = err;
-    va_end(va);
     return -1;
 }
 
